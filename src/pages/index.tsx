ce15c193658480aa23bd0b675939a98f5ea5be01
Marcio Costa Rodrigues <marciodeth@hotmail.com>
2023-07-03 00:36:47 -0300

    Componente tabela e inclusao de icones

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,23 @@
 import Layout from "@/components/Layout";
+import Tabela from "@/components/Tabela";
+import Cliente from "@/core/models/Cliente";
 
 export default function Home() {
+  const clientes = [
+    new Cliente("Ana", 34, "1"),
+    new Cliente("Bia", 45, "2"),
+    new Cliente("Carlos", 23, "3"),
+    new Cliente("Pedro", 54, "4"),
+  ];
+
+  function clienteSelecionado(cliente: Cliente) {
+    console.log(cliente);
+  }
+
+  function clienteExcluido(cliente: Cliente) {
+    console.log(cliente);
+  }
+
   return (
     <div
       className={`
@@ -10,7 +27,11 @@ export default function Home() {
     `}
     >
       <Layout titulo="Cadastro Simples">
-        <span>Conteudo</span>
+        <Tabela
+          clientes={clientes}
+          clienteSelecionado={clienteSelecionado}
+          clienteExcluido={clienteExcluido}
+        ></Tabela>
       </Layout>
     </div>
   );
